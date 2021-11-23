c5ccd855706c48245447a0569ffdd91c56edbc94
Jared Holgate <jared.holgate@hashicorp.com>
2021-11-23 22:34:42 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "ssh_key" {
-  value = module.stack_azure_hashicorp_vault.ssh_key
+  value = module.stack_azure_virtual_machine_example.ssh_key
 }
 
 output "demo_password" {
-  value = module.stack_azure_hashicorp_vault.demo_password
+  value = module.stack_azure_virtual_machine_example.demo_password
 }
 
 output "demo_public_ip_address" {
-  value = module.stack_azure_hashicorp_vault.demo_public_ip_address
+  value = module.stack_azure_virtual_machine_example.demo_public_ip_address
 }
