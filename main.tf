abfe82ec5c1326f4096fc758e44f7214961be129
Jared Holgate <jared.holgate@hashicorp.com>
2021-11-23 21:30:00 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ provider "azurerm" {
 }
 
 module "stack_azure_virtual_machine_example" {
-  source                       = "app.terraform.io/jared-holgate-hashicorp/stack_azure_hashicorp_vault/jaredholgate"
+  source                       = "app.terraform.io/jaredfholgate-hashicorp/stack_azure_virtual_machine_example/jaredholgate"
   resource_group_name          = format("%s%s", var.resource_group_name_prefix, var.deployment_environment)
   primary_cluster_size         = 3 
   secondary_cluster_size       = 3
